00c496c7a13fbb4d5801cfa5c6f7010200b35834
Mateusz <1705931@brunel.ac.uk>
2020-11-16 21:56:29 +0000

    Reorganise the structure of the directories

---
+++ pythonScripts/repo_downloader.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+input_file=$1
+echo "Input file:" $input_file
+ 
+repo_links="$(grep '[^-|\?\?]$' $input_file | cut -f2 -d';')"
+echo "repo_links:" $repo_links
+
+while read -r repo; do 
+    repo_name="$(echo $repo | cut -f4- -d'/' | tr '/' '_' | sed 's/_$//')"
+	echo "repo_name" $repo_name
+
+    if [[ $repo == *".git"* || $repo == *"git."* || $repo == "git:"* ]]; then
+        echo "Cloning git $repo_name repository [$repo]"
+		#Uncomment the following line to clone repositories
+		#git clone $repo >"$repo_name log.txt"
+        echo "finished."
+        :
+    elif [[ $repo == *".hg"* || $repo == *"hg."* || $repo == *"bitbucket."* || $repo == *"/hg/"* ]]; then
+        echo "Cloning mercurial $repo_name repository [$repo]"
+        hg clone $repo $repo_name 2>"$repo_name log.txt"
+        echo "finished."
+        :
+    elif [[ $repo == *".svn"* || $repo == *"svn."* || $repo == "svn:"* ]]; then
+        echo "Cloning svn $repo_name repository [$repo]"
+        svn checkout $repo $repo_name 2>"$repo_name log.txt"
+        echo "finished."
+        :
+    elif [[ $repo == *".cvs"* || $repo == *"cvs."* || $repo == *"//cvs"* ]]; then
+        echo "Cloning cvs not yet implemented."
+        # cvs checkout -Qd ./ $repo $repo_name
+        # echo "finished."
+        :
+    else 
+        echo "Unknown repo: $repo"
+    fi
+
+done <<<"$repo_links"
